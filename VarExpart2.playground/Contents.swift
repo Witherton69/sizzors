2773baa9694976b2eb92cec94249dde5baddd56f
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-02-27 08:55:07 -0600

    Var ex

---
+++ VarExpart2.playground/Contents.swift
@@ -0,0 +1,42 @@
+// VARIABLES EXERCISE
+// 1
+print("Witherton")
+
+
+// 2
+// String, Character, Bool, Int, Float, Double
+
+
+// 3
+// let
+
+
+// 4
+var person = "Evan"
+print(person)
+
+
+// 5
+person = "Kyle"
+print(person)
+
+
+// BONUS
+// 1
+var number = 3
+print(number)
+
+
+// 2
+number = number + 2
+print(number)
+
+
+// 3
+number += 5
+print(number)
+
+
+// 4
+var nums = [1, 8, 9]
+print(nums)
