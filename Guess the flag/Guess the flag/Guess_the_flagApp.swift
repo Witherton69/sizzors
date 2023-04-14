a9ae751256f71bbea0bc3101027576bcd89504b4
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-04-14 08:23:17 -0500

    rock paper please work

---
+++ Guess the flag/Guess the flag/Guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_flagApp.swift
+//  Guess the flag
+//
+//  Created by Katelyn Gosnell on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
